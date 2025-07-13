33fc5e2ff16e61c2d30f5ac7477cf7663bfa7efb
extortionist <birkbjerke@gmail.com>
2025-07-13 23:14:40 +0200

    -

--- supabase/migrations/20240321000006_fix_transaction_types.sql
+++
@@ -1,8 +0,0 @@
--- Drop existing constraint
-ALTER TABLE bank_transactions 
-    DROP CONSTRAINT IF EXISTS valid_transaction_type;
-
--- Add new constraint with correct transaction types
-ALTER TABLE bank_transactions 
-    ADD CONSTRAINT valid_transaction_type 
-    CHECK (transaction_type IN ('deposit', 'withdraw', 'transfer', 'interest', 'fee')); 
